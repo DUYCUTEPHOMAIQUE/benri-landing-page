8f7acc468feca231b74a9e8358d430178dbed022
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-10-12 14:36:40 +0700

    OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,12 @@ function Header() {
   <nav className="container mx-auto px-6 py-3">
     <div className="flex justify-between items-center">
       <a href="#h" className="text-2xl font-bold text-benri-green-400">Benri Fake</a>
-      <div className="hidden md:flex items-center space-x-4">
-        <a href="#features" className="text-gray-800 hover:text-benri-green-400">Tính năng</a>
-        <a href="#testimonials" className="text-gray-800 hover:text-benri-green-400">Đánh giá</a>
-        <a href="#faq" className="text-gray-800 hover:text-benri-green-400">FAQ</a>
+      <div className="flex items-center space-x-4">
+        <div className="hidden md:flex items-center space-x-4">
+          <a href="#features" className="text-gray-800 hover:text-benri-green-400">Tính năng</a>
+          <a href="#testimonials" className="text-gray-800 hover:text-benri-green-400">Đánh giá</a>
+          <a href="#faq" className="text-gray-800 hover:text-benri-green-400">FAQ</a>
+        </div>
         <a href="#" className="bg-benri-green-400 text-white px-4 py-2 rounded-full hover:bg-benri-green-500 transition duration-300">Tải ứng dụng</a>
       </div>
     </div>

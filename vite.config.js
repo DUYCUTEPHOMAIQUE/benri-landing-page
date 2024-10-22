a5cd18942bfbd1dc6f5ae3dbb4ecd72b266855b7
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-10-22 16:24:39 +0700

    Duy deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/benri-landing-page/',
+  base: '/benri-landing-page-home/',
   plugins: [react()],
 })
